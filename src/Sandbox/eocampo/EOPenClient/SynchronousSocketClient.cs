4b1bf81971a1b35e22debaaf164ac31844e10635
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2017-06-02 12:31:10 -0500

    Socket Listener/Client sample.

---
+++ src/Sandbox/eocampo/EOPenClient/SynchronousSocketClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EOPenClient
+{
+    public class SynchronousSocketClient
+    {
+        public static void StartClient() {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                //IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName())  
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(""); // AddressFamily.InterNetwork
+                //IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPAddress ipAddress = null;
+                for (int i = 0; i < ipHostInfo.AddressList.LongLength; i++) {
+                    if (ipHostInfo.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                        ipAddress = ipHostInfo.AddressList[i];
+                }
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 4510);
+
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.  
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.  
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane) {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se) {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ src/Sandbox/eocampo/EOPenServer/AsynchronousSocketListener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EOPenServer
+{
+    internal class AsynchronousSocketListener
+    {
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public void Start() {
+            var permission = new SocketPermission(NetworkAccess.Accept,
+                       TransportType.Tcp, "", SocketPermission.AllPorts);
+
+            IPHostEntry ipHost = Dns.GetHostEntry(""); // AddressFamily.InterNetwork
+            IPAddress ipAddr = null;
+            for (int i = 0; i < ipHost.AddressList.LongLength; i++) {
+                if (ipHost.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                    ipAddr = ipHost.AddressList[i];
+            }
+            var ipEndPoint = new IPEndPoint(ipAddr, 4510);
+            Console.WriteLine("IP Address: " + ipAddr.ToString());
+
+            //Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp); //  ipAddr.AddressFamily
+
+            //try {
+            //    listener.Bind(ipEndPoint);
+            //    listener.Listen(100);
+
+            //    while (true) {
+            //        // Set the event to nonsignaled state.  
+            //        allDone.Reset();
+
+            //        // Start an asynchronous socket to listen for connections.  
+            //        Console.WriteLine("Waiting for a connection...");
+            //        listener.BeginAccept(
+            //            new AsyncCallback(AcceptCallback),
+            //            listener);
+
+            //        // Wait until a connection is made before continuing.  
+            //        allDone.WaitOne();
+            //    }
+
+            //}
+            //catch (Exception e) {
+            //    Console.WriteLine(e.ToString());
+            //} 
+        }
+    }
+}
